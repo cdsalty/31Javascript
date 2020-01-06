b9e5afc3fbf246bec1f051688b58d30149a74ffc
cdsalty <soltiscd@gmail.com>
2020-01-05 23:56:45 -0500

    working on Weather Aplication

---
+++ JS_Weather_App/weather_scripts.js
@@ -0,0 +1,37 @@
+console.log('sanity-check');
+
+/*
+- will be using the dark sky api along with reverse geolocation api
+- responsive via bootstrap
+- nice ui for portfolio representation
+
+*/
+
+const wDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const wMonth = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const iconValue = {
+  CLEARDAY: 'clear-day',
+  CLEARNIGHT: 'clear-night',
+  RAIN: 'rain',
+  SNOW: 'snow',
+  SLEET: 'sleet',
+  WIND: 'wind',
+  FOG: 'fog',
+  CLOUDY: 'cloudy',
+  PARTLY_CLOUDY_DAY: 'partly-cloudy-day',
+  PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night'
+};
