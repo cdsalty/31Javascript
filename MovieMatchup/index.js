18f72b6add9a5017f78bfabcdb5e44e3f1f21c0d
cdsalty <soltiscd@gmail.com>
2020-02-07 17:41:26 -0500

    extracted debounce into seperate file

--- MovieMatchup/index.js
+++ MovieMatchup/index.js
@@ -13,19 +13,7 @@ const fetchData = async (searchTerm) => {
 // 2. Start on autocomplete search by using the DEBOUNCE APPROACH: I did not know this previously
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
-
 const onInput = (event) => {
   fetchData(event.target.value);
 };
-input.addEventListener('input', debounce(onInput, 500));
+input.addEventListener('input', debounce(onInput, 800));
