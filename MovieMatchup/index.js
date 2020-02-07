a16fbede2d9d4c1b62659edc54faea8241088dbf
cdsalty <soltiscd@gmail.com>
2020-02-07 17:35:41 -0500

    debounce approach working; first use with it

--- MovieMatchup/index.js
+++ MovieMatchup/index.js
@@ -1,48 +1,31 @@
+// 1. Fetch the data
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      // optional params:
       apikey: 'e8015927',
-      // s: 'tangled'  // returns movie name from a search which is updated and referenced to as searchTerm
-      // 'i' is in place for imbDB and contains all the informative data
-      // i: 'tt0398286'
-      i: searchTerm
+      s: searchTerm
     }
   });
-  // console.log(response);
-  console.log(response.data); // the search input being returned. I omited this by mistake and nothing would return.
+
+  console.log(response.data);
 };
 
-// fetchData();
-
-// 2. Autocomplete search using DEBOUNCE
+// 2. Start on autocomplete search by using the DEBOUNCE APPROACH: I did not know this previously
 const input = document.querySelector('input');
 
-// function for user's search input
-const debounce = (func) => {
-  let timeoutId; // each setTimeout duration...
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
   return (...args) => {
-    // return all arguements such as arg1, arg2, etc. the same thing...
     if (timeoutId) {
       clearTimeout(timeoutId);
     }
     timeoutId = setTimeout(() => {
-      func.apply(null, args); // each iteration will have a new value
-    }, 800);
+      func.apply(null, args);
+    }, delay);
   };
 };
 
-const onInput = (e) => {
-  fetchData(e.target.value);
+const onInput = (event) => {
+  fetchData(event.target.value);
 };
-
-input.addEventListener('input', debounce(onInput));
-/* 
-delaying the user's search input in order to make less calls to the api
-- the key to making this start is by assigning a varaible to the setTimeout function so we can control
-  when it's called. 
-
-  timeoutId = setTimeout(() => {
-    fetchData(e.target.value);
-  }, 1000);
-*/
+input.addEventListener('input', debounce(onInput, 500));
--- MovieMatchup/indexNotes.js
+++ MovieMatchup/indexNotes.js
@@ -113,10 +113,113 @@ const onInput = (e) => {
 
 input.addEventListener('input', onInput);
 
+
+updated:
+const input = document.querySelector('input');
+
+let timeoutId; // represents each setTimeout duration...
+
+// function for user's search input
+const onInput = (e) => {
+  if (timeoutId) {
+    // setTimeout, on each each call also has an id assigned to it. timeoutId represents this.
+    // not defined on first run through; will be defined on the second iteration
+    clearTimeout(timeoutId); // once it's called, the new timeoutId will have a new value; pattern repeated
+  }
+
+  // logic to prevent user from making too many calls to the api.
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value); // each iteration will have a new value
+  }, 800);
+};
+
+input.addEventListener('input', onInput);
+/* 
+delaying the user's search input in order to make less calls to the api
+- the key to making this start is by assigning a varaible to the setTimeout function so we can control
+  when it's called. 
+
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value);
+  }, 1000);
 */
 
 // ----------------------------------------------------------------------------------------------------
 /*
+Updated using DEBOUNCE
+const fetchData = async (searchTerm) => {
+//   const response = await axios.get('http://www.omdbapi.com/', {
+//     params: {
+//       // optional params:
+//       apikey: 'e8015927',
+//       // s: 'tangled'  // returns movie name from a search which is updated and referenced to as searchTerm
+//       // 'i' is in place for imbDB and contains all the informative data
+//       // i: 'tt0398286'
+//       s: searchTerm
+//     }
+//   });
+//   // console.log(response);
+//   console.log(response.data); // the search input being returned. VERY IMPORTANT!!!
+// };
+
+// // fetchData();
+
+// // 2. Start on autocomplete search
+// const input = document.querySelector('input');
+
+// let timeoutId; // defined but no value
+
+// // function for user's search input
+// const onInput = (e) => {
+//   // will be called several times.
+//   // on first search...
+//   if (timeoutId) {
+//     // won't be defined until the second iteration
+//     // check to see if timeoutId is defined; the first time it won't be so it will be skipped
+//     // on the user's SECOND search input, the timeoutId will be defined.
+//     clearTimeout(timeoutId);
+//     // when it's called stopped, the new timeoutId will have a new value below and then repeat the pattern
+//   }
+//   timeoutId = setTimeout(() => {
+//     fetchData(e.target.value);
+//   }, 1000);
+// };
+
+// input.addEventListener('input', onInput);
+
+Succinctly:
+// 1. Fetch the data
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'e8015927',
+      s: searchTerm
+    }
+  });
+
+  console.log(response.data);
+};
+
+// 2. Start on autocomplete search by using the DEBOUNCE APPROACH: I did not know this previously
+const input = document.querySelector('input');
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));
+
 
 */
 
