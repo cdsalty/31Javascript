fece69f13e156632a8eefee19fb810725b77d62f
cdsalty <soltiscd@gmail.com>
2020-02-11 23:16:13 -0500

    setup the api call with async/await, successfully

--- youtube_search/src/components/App.js
+++ youtube_search/src/components/App.js
@@ -3,19 +3,25 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
+  state = {videos: []}; // will update with response.data.items (which holds the search results)
+
+  onTermSubmit = async (term) => {
     // console.log(term); // verify that when user submits search, the term is being captured
     // use the term to make a request from the youtube api by importing youtube.js and axios
-    youtube.get('/search', {
+    // youtube.get('/search', {
+    const response = await youtube.get('/search', {
       params: {
         q: term // the term the user submitted
       }
     });
+    // console.log(response.data.items);
+    this.setState({videos: response.data.items}); // update state with the search results
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        Displaying {this.state.videos.length} videos.
       </div>
     );
   }
