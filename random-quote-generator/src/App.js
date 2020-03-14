0a0514306a10691eb3fe2aafabcc53de47caa36c
cdsalty <soltiscd@gmail.com>
2020-03-14 19:44:00 -0400

    quick setup for creating random quote generator

---
+++ random-quote-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="quote-box">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
