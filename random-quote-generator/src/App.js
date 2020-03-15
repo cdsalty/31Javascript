1f8a43ddb3fd55ad050334ac8a0d6a9a13bc0b7b
cdsalty <soltiscd@gmail.com>
2020-03-15 16:25:44 -0400

    getting quotes, next to get randomSelectedQuote

--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -4,11 +4,35 @@ import Button from './components/Button';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null
+    };
+  }
+
+  // Get Random Quote Data
+  componentDidMount() {
+    fetch(
+      'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
+    )
+      .then((res) => res.json()) // go through response and parse the body/data
+      // .then((data) => console.log(data)); // verify the data is coming in. (Layout is a list of quotes)
+      // read note at bottom
+      .then((data) =>
+        this.setState({
+          quotes: data // verify in the render
+        })
+      );
+  }
+
   handleNextClick = () => {
     console.log('Successful Connection');
   };
 
   render() {
+    console.log(this.state.quotes); // CONFIRMING DATA BEING SETUP
     return (
       <div className="App" id="quote-box">
         <Button handleClick={this.handleNextClick} buttonDisplayName="Next Quote" />
@@ -18,3 +42,23 @@ class App extends Component {
 }
 
 export default App;
+
+// https://codepen.io/freeCodeCamp/full/qRZeGZ
+
+/*
+
+my way v.s. the suggest route
+
+.then((data) =>
+  this.setState({
+    quotes: data
+  });
+
+  *Suggested 
+.then((quotes) =>){
+  this.setState({
+    quotes: quotes
+  });
+}
+
+*/
